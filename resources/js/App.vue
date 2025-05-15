255f92ec9384b7008e4071e9092982fc605cb8dd
Jonathan Prada <jstevenprada@gmail.com>
2025-05-15 14:08:34 -0500

    First commit

---
+++ resources/js/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-container>
+        <h1>Hola desde Vuetify 2 😎</h1>
+        <v-btn color="primary">Botón de prueba</v-btn>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+</script>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+    <title>Landing</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body>
+    <div id="app"></div>
+</body>
+</html>
